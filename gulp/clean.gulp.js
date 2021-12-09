7999e701622176bfd9e65f5b4f76e3e73d233a10
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2021-12-09 13:14:34 +0600

    button, color and map added

--- gulp/clean.gulp.js
+++ gulp/clean.gulp.js
@@ -6,12 +6,6 @@ const { paths, baseDir, version } = require('./utils.js');
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 |  Clean
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
-gulp.task('clean', () =>
-  del([
-    `${baseDir}/${paths.style.dest}/**/*.*`,
-    `${baseDir}/${paths.script.dest}/**/*.*`,
-    `${baseDir}/**/*.html`,
-  ])
-);
+gulp.task('clean', () => del([`${baseDir}/${paths.style.dest}/**/*.*`, `${baseDir}/${paths.script.dest}/**/*.*`, `${baseDir}/**/*.html`]));
 gulp.task('clean:build', () => del(paths.dir.prod));
 gulp.task('clean:live', () => del(`live/${version}`));
--- src/js/dropdown-on-hover.js
+++ src/js/dropdown-on-hover.js
@@ -3,27 +3,27 @@
 /* -------------------------------------------------------------------------- */
 
 const dropdownOnHover = () => {
-  const navbarArea = document.querySelector("[data-top-nav-dropdowns]");
+	const navbarArea = document.querySelectorAll('[data-bs-toggle="dropdown"]');
 
-  if (navbarArea) {
-    navbarArea.addEventListener("mouseover", (e) => {
-      if (
-        e.target.className.includes("dropdown-toggle") &&
-        window.innerWidth > 992
-      ) {
-        const dropdownInstance = new window.bootstrap.Dropdown(e.target);
-        
-        /* eslint-disable no-underscore-dangle */
-        dropdownInstance._element.classList.add('show')
-        dropdownInstance._menu.classList.add('show')
-        dropdownInstance._menu.setAttribute('data-bs-popper', 'none')
-        
-        e.target.parentNode.addEventListener("mouseleave", () => {
-          dropdownInstance.hide();
-        });
-      }
-    });
-  }
+	if (navbarArea) {
+		navbarArea.forEach(navbarItem => {
+			console.log(navbarItem.dataset['bsToggle']);
+			navbarItem.addEventListener('mouseover', e => {
+				if (e.target.className.includes('dropdown-toggle') && window.innerWidth > 992) {
+					const dropdownInstance = new window.bootstrap.Dropdown(e.target);
+
+					/* eslint-disable no-underscore-dangle */
+					dropdownInstance._element.classList.add('show');
+					dropdownInstance._menu.classList.add('show');
+					dropdownInstance._menu.setAttribute('data-bs-popper', 'none');
+
+					e.target.parentNode.addEventListener('mouseleave', () => {
+						dropdownInstance.hide();
+					});
+				}
+			});
+		});
+	}
 };
 
 export default dropdownOnHover;
